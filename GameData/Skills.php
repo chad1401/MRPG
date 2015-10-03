41a4462372718db75794233aba79783eb0fcc458
Minecraftlog21-Master <Minecraftlog21@gmail.com>
2015-10-03 17:10:17 -0400

    Master Computer Test
    yep....

--- GameData/Skills.php
+++ GameData/Skills.php
@@ -1,5 +1,5 @@
 <?php
-	$SkillName = ["PickLocking","Bardering","ShopLifting","Trading"];
+	$SkillName = ["PickLocking","Bardering","ShopLifting","Trading",""];
 	$SkillCost = [25,30,50,50];
 	$SkillMinLevel = [10,5,5,15];
 	$SkillUpgradeCostMultplier = [2,3,2,2];
